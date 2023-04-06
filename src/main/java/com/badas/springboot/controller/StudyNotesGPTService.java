d1fa1b5b42bf75044262851d6a80dfac143ee449
Gabriel Badaró <grbadas@gmail.com>
2023-04-06 20:31:26 -0300

    Chat gpt api

---
+++ src/main/java/com/badas/springboot/controller/StudyNotesGPTService.java
@@ -0,0 +1,62 @@
+	package com.badas.springboot.controller;
+	
+	import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+	import org.springframework.stereotype.Service;
+	import org.springframework.web.reactive.function.client.WebClient;
+	
+	import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+	import com.fasterxml.jackson.databind.annotation.JsonNaming;
+	
+	import reactor.core.publisher.Mono;
+	
+	@Service
+	public class StudyNotesGPTService {
+		private WebClient webClient;
+		
+		public StudyNotesGPTService(WebClient.Builder builder, 
+				@Value("${openai.api.key}") String apiKey) {
+			this.webClient = builder.
+					baseUrl("https://api.openai.com/v1/engines/davinci-codex/completions").
+					defaultHeader("Content-Type", "application/json").
+					defaultHeader("Authorization", String.format("Bearer %s", apiKey)).
+					build();
+		}
+	
+		public Mono<ChatGPTResponse> createStudyNotes(String topic) {
+			
+			ChatGPTRequest request = createStudyRequest(topic);
+			
+			return webClient.post().bodyValue(request)
+					.retrieve()
+					.bodyToMono(ChatGPTResponse.class);
+			
+		}
+		
+		private ChatGPTRequest createStudyRequest(String topic) {
+			String question = "Quais são os pontos chave que devo estudar sobre o seguinte assunto : " + 
+		topic;
+			
+			return new ChatGPTRequest("text-davinci-003", question, 0.3, 2000, 1.0, 0.0, 0.0);
+		}
+	
+		
+		@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+		record ChatGPTRequest(String model, String prompt,
+				Double temperature, Integer maxTokens, Double topP,
+				Double frequencyPenalty, Double presencePenalty) {
+			
+			
+			
+		}
+		
+		record ChatGPTResponse(List<Choice> choices) {
+			
+		}
+		
+		record Choice(String text) {
+			
+		}
+		
+	}
